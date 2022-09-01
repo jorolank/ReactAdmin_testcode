b78ac7939f9b3e52a6dc04d85f41f8647770aeef
blckclov3r <blckclov3r@gmail.com>
2022-09-01 22:26:05 +0800

    add server

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+/* 
+ youtube url: https://www.youtube.com/watch?v=HRmdj-HpJyE
+ youtube channel: traversy media
+*/
+function App() {
+
+  return (
+    <div className="App">
+        <h1>Helloworld</h1>
+    </div>
+  )
+}
+
+export default App
