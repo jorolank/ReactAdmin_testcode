2c3b162c597d15eac0fd073ee9ed051f9049b9b1
blckclov3r <blckclov3r@gmail.com>
2022-09-09 23:38:06 +0800

    commit

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,12 +1,37 @@
 import { Card } from "@material-ui/core";
 import CardContent from "@material-ui/core/CardContent";
 import CardHeader from "@material-ui/core/CardHeader";
+import { Button } from "@mui/material";
+import { useResetStore, useStore, useStoreContext } from "react-admin";
 
 const Dashboard = () =>{
+
+    const [helpOpen,setHelpOpen] = useStore('help.open',false);
+    const reset = useResetStore();
+    const store = useStoreContext();
+
+    const setOpen = () =>{
+        setHelpOpen(v => !v)
+    }
+    const resetStore = ()=>{
+        reset();
+    }
+
+    const storeContext = () =>{
+        console.log(store.getItem('help.open',false))
+    }
     return (
         <Card>
             <CardHeader title="Welcome to the administration" />
-            <CardContent>Lorem ipsum sic dolor amet...</CardContent>
+            <CardContent>
+               <h1>Helloworld</h1>
+               <div style={{display: 'flex',gap: '15px'}}>
+               <Button onClick={setOpen} variant="contained">{helpOpen ? 'Show' : 'Hide'} Help</Button>
+               <Button onClick={resetStore} variant="contained">Reset</Button>
+               <Button onClick={storeContext} variant="contained">Store Context</Button>
+               </div>
+            </CardContent>
+       
         </Card>
     )
 }
